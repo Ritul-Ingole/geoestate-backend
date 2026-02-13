83024652253b1c66bb272e5e6d6a90e5dcbe8a8f
Ritul-Ingole <ritulmi007@gmail.com>
2026-02-14 00:34:26 +0530

    changes in authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -36,7 +36,7 @@ exports.login = async (req, res) => {
   const token = jwt.sign(
     { id: user._id },
     process.env.JWT_SECRET,
-    { expiresIn: "15m" }
+    { expiresIn: "15d" }
   );
 
   res.json({
