b0eb2d202b458d852a9c02cb57664f2887f29822
Ritul-Ingole <ritulmi007@gmail.com>
2026-05-20 19:51:33 +0530

    CLIENT_URL field added in the .env file

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use("/uploads", express.static("uploads"));
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: process.env.CLIENT_URL,
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
