a892fe71658129e58c3005cd3d963acc67ced4ba
Ritul-Ingole <ritulmi007@gmail.com>
2026-05-20 23:14:20 +0530

    Fix production CORS

--- index.js
+++ index.js
@@ -21,7 +21,9 @@ app.use(cors({
       callback(new Error("Not allowed by CORS"));
     }
   },
-  credentials: true
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 }));
 app.options("/{*path}", cors());
 app.use(express.json());
